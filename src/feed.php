3e03a843839143d89496944722414e24d48e4c14
Andy Meneely <andy.meneely@gmail.com>
2011-09-25 22:11:08 -0400

    php only lists m4v files

--- src/feed.php
+++ src/feed.php
@@ -4,11 +4,17 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>";
 echo "<videos>";
 if ($handle = opendir('.')) {
     while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != "..") {
+        if ($file != "." && $file != ".." && endsWith($file,".m4v")) {
             echo "<video file=\"$file\" ></video>";
         }
     }
     closedir($handle);
 }
 echo "</videos>";
+
+function endsWith($haystack, $needle) {
+    $start  = strlen($needle) * -1; //negative
+    return (substr($haystack, $start) === $needle);
+}
+
 ?>
